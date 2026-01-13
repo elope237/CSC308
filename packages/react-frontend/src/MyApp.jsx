fea9f3b361671c514bfa80a8b9f5abd686b209aa
elope237 <elope237@calpoly.edu>
2026-01-13 14:15:13 -0800

    Finish steps 7-9

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,25 +1,24 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    { name: "Charlie", job: "Janitor" },
-    { name: "Mac", job: "Bouncer" },
-    { name: "Dee", job: "Aspiring actress" },
-    { name: "Dennis", job: "Bartender" },
-  ]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
-    // Best practice: use a functional update so we always use the latest state
-    setCharacters((prevCharacters) =>
-      prevCharacters.filter((_, i) => i !== index)
-    );
+    const updated = characters.filter((_, i) => i !== index);
+    setCharacters(updated);
+  }
+
+  function updateList(person) {
+    setCharacters([...characters, person]);
   }
 
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
     </div>
   );
 }
